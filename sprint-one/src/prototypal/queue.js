d565ba20889dec74a2d50084a36eef0e94d64430
Matt Hong <matthew.m.hong@gmail.com>
2020-08-20 15:42:57 -0700

    prototypal stack and queue complete with passing tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  var index = this.size();
+  this.storage[index + 1] = value;
+};
 
+queueMethods.dequeue = function() {
+  var size = this.size();
+  var value;
+  for (var i = 1; i <= size; i++) {
+    if (i === 1) {
+      value = this.storage[i];
+    }
+    if (!this.storage[i + 1]) {
+      this.storage[i] = undefined;
+    } else {
+      this.storage[i] = this.storage[i + 1];
+    }
+  }
+
+  delete this.storage[size];
+  return value;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,7 +1,7 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var newStack = Object.create(Stack.stackMethods);
+  var newStack = Object.create(stackMethods);
   newStack.storage = {};
 
   return newStack;
@@ -9,18 +9,18 @@ var Stack = function() {
 
 var stackMethods = {};
 
-// someInstance.push = function(value) {
-//   var index = someInstance.size();
-//   storage[index + 1] = value;
-// };
+stackMethods.push = function(value) {
+  var index = this.size();
+  this.storage[index + 1] = value;
+};
 
-// so meInstance.pop = function() {
-//   var index = someInstance.size();
-//   var value = storage[index];
-//   delete storage[index];
-//   return value;
-// };
+stackMethods.pop = function() {
+  var index = this.size();
+  var value = this.storage[index];
+  delete this.storage[index];
+  return value;
+};
 
-// someInstance.size = function() {
-//   return Object.keys(storage).length;
-// };
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
