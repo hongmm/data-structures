4fe0528128304a63f75d207e90e8897458ff3e33
Matt Hong <matthew.m.hong@gmail.com>
2020-08-20 16:17:03 -0700

    Sprint 1 Assignment Complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    var index = this.size();
+    this.storage[index + 1] = value;
+  }
+
+  dequeue() {
+    var index = this.size();
+    var first;
+    for (var i = 1; i <= index; i++) {
+      if (i === 1) {
+        first = this.storage[i];
+      }
+      this.storage[i] = this.storage[i + 1];
+    }
+    delete this.storage[index];
+    return first;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var newQueue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    var index = this.size();
+    this.storage[index + 1] = value;
+  }
+
+  pop() {
+    var index = this.size();
+    var value = this.storage[index];
+    delete this.storage[index];
+    return value;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var newStack = new Stack();
