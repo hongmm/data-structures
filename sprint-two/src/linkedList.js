3f48ef11380871cb63a32431e362aa54bb92e56c
Matt Hong <matthew.m.hong@gmail.com>
2020-08-20 17:14:51 -0700

    completed linkedList with passing tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,35 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.storage = {};
 
   list.addToTail = function(value) {
+    var listSize = Object.keys(this.storage).length;
+    var newNode = Node(value);
+    if (listSize === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode.value;
+      this.tail = newNode;
+    }
+    this.storage[value] = newNode;
   };
 
   list.removeHead = function() {
+    var headToRemove = this.head;
+    var nextInLine = this.head.next;
+    this.head = this.storage[nextInLine];
+
+    delete this.storage[headToRemove.value];
+    return headToRemove.value;
   };
 
   list.contains = function(target) {
+    if (this.storage[target]) {
+      return true;
+    }
+    return false;
   };
 
   return list;
