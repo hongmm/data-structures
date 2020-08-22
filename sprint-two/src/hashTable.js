03f7e928c77560e060b0742ab47339b28b90bbda
Matt Hong <matthew.m.hong@gmail.com>
2020-08-22 14:33:58 -0700

    hash table complete with working tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,42 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      var prevValue = tuple[1];
+      tuple[1] = v;
+      return prevValue;
+    }
+  }
+  bucket.push([k,v]);
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i,1);
+      this._storage.set(index, bucket);
+    }
+  }
 };
 
 
